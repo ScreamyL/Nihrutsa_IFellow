b2967b6755cd3cc33f2d1f598ea92d7675d1008d
ScreamyL <evklid999@gmail.com>
2024-09-23 15:36:52 +0300

    HW branch, добавил нужную зависимость, всё запускается из тест класса

--- src/test/java/tests/CucumberRunnerTest.java
+++ src/test/java/tests/CucumberRunnerTest.java
@@ -7,14 +7,15 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("steps")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "feature")
+@SelectClasspathResource("steps, hooks")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps, hooks")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/tests/feature")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, json:target/cucumber.json")
 
-public class CucumberRunnerTest {
+public final class CucumberRunnerTest {
 }
