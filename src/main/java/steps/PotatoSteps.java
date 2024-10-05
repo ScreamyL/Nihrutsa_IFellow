d1babaf76294c61439ec2c9c8e19956783391cb0
ScreamyL <evklid999@gmail.com>
2024-10-05 18:30:48 +0300

    HW branch, поправил, а то какую-то херь спросони сделал

--- src/main/java/steps/PotatoSteps.java
+++ src/main/java/steps/PotatoSteps.java
@@ -1,11 +1,14 @@
 package steps;
 
-
 import api.PotatoApi;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.Props.props;
 
 public class PotatoSteps {
 
@@ -16,17 +19,28 @@ public class PotatoSteps {
     }
 
     public void performPotatoTest() {
-        Response response = createPotato("Tomato", "Eat maket");
+        Response response = createPotatoFromFile("Tomato", "Eat maket");
+
+        assertEquals(201, response.getStatusCode(), "Статус ответа не соответствует 201!");
+
         JSONObject jsonResponse = new JSONObject(response.asString());
 
         assertEquals("Tomato", jsonResponse.getString("name"), "Имя не соответствует ожидаемому значению!");
         assertEquals("Eat maket", jsonResponse.getString("job"), "Работа не соответствует ожидаемому значению!");
     }
 
-    public Response createPotato(String name, String job) {
-        JSONObject body = new JSONObject();
-        body.put("name", name);
-        body.put("job", job);
-        return api.createPotato(body);
+    public Response createPotatoFromFile(String name, String job) {
+        try {
+            String content = new String(Files.readAllBytes(Path.of(props.file_path())));
+            JSONObject body = new JSONObject(content);
+
+            body.put("name", name);
+            body.put("job", job);
+
+            return api.createPotato(body);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Ошибка при чтении файла: " + e.getMessage());
+        }
     }
 }
