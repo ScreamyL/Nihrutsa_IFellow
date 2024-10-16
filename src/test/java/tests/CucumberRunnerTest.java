70ee045b6776a9eb455df0960ce1f9a97ba16c68
ScreamyL <evklid999@gmail.com>
2024-10-16 18:52:26 +0300

    reformat

--- src/test/java/tests/CucumberRunnerTest.java
+++ src/test/java/tests/CucumberRunnerTest.java
@@ -16,7 +16,8 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/tests/feature")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@restassured")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, json:target/cucumber.json")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, json:target/cucumber.json")
 
 public final class CucumberRunnerTest {
 }
